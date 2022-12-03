238114dcd9ac241a35a678443e4dd47c5f8887ff
kevin <kevinsef94@gmail.com>
2022-12-03 21:49:02 +0800

    pizza card and product page interface

--- component/PizzaCard.jsx
+++ component/PizzaCard.jsx
@@ -1,7 +1,17 @@
 import styles from "../styles/PizzaCard.module.css";
+import Image from "next/image";
 
 const PizzaCard = () => {
-  return <div className={styles.container}>PizzaCard</div>;
+  return (
+    <div className={styles.container}>
+      <Image src="/img/pizza.png" alt="" width="250" height="250" />
+      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <span className={styles.price}>$19.90</span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+      </p>
+    </div>
+  );
 };
 
 export default PizzaCard;
---
+++ pages/product/[id].jsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../../styles/Product.module.css";
+
+const Product = () => {
+  const [size, setSize] = useState(0);
+  const pizza = {
+    id: 1,
+    img: "/img/pizza.png",
+    name: "CAMPAGNOLA",
+    price: [19.9, 23.9, 27.9],
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis corporis autem vitae quisquam praesentium necessitatibus. Eligendi aliquam impedit aperiam eos corporis inventore dignissimos, iste laborum sunt, earum officia tenetur totam.",
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.imgContainer}>
+          <Image src={pizza.img} alt="" layout="fill" objectFit="contain" />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.name}</h1>
+        <span className={styles.price}>${pizza.price[size]}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className={styles.choose}>Choose the size</h3>
+        <div className={styles.sizes}>
+          <div className={styles.size} onClick={() => setSize(0)}>
+            <Image src="/img/size.png" alt="pizza size" layout="fill" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(1)}>
+            <Image src="/img/size.png" alt="pizza size" layout="fill" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(2)}>
+            <Image src="/img/size.png" alt="pizza size" layout="fill" />
+            <span className={styles.number}>Large</span>
+          </div>
+        </div>
+        <h3 className={styles.choose}>Choose additional ingredients</h3>
+        <div className={styles.ingredients}>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              name="double"
+              id="double"
+              className={styles.checkbox}
+            />
+            <label htmlFor="double">Double Ingredients</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              name="cheese"
+              id="cheese"
+              className={styles.checkbox}
+            />
+            <label htmlFor="cheese">Extra Cheese</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              name="spicy"
+              id="spicy"
+              className={styles.checkbox}
+            />
+            <label htmlFor="spicy">Spicy Sauce</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              name="garlic"
+              id="garlic"
+              className={styles.checkbox}
+            />
+            <label htmlFor="garlic">Garlic Sauce</label>
+          </div>
+        </div>
+        <div className={styles.add}>
+          <input type="number" defaultValue={1} className={styles.quantity} />
+          <button className={styles.button}>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
